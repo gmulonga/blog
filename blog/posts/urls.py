e34a3650a035b7331dbc88d2631e354d56069e8a
gmulonga <gadsonmulonga@gmail.com>
2024-08-13 17:17:57 +0300

    adding routes for posts

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,7 +1,12 @@
-from . import views
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('', views.post_home, name=''),
+    path('', views.post_detail, name='post_detail'),
+    path('detail/', views.post_list, name='post_list'),
+    path('create/', views.post_add, name='post_create'),
+    path('update/', views.post_update, name='post_update'),
+    path('delete/', views.post_delete, name='post_delete'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def post_home(request):
-    return HttpResponse("<h1>hello World!</h1>")
+def post_detail(request):
+    return HttpResponse("<h1>Home</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>Post list</h1>")
+
+def post_add(request):
+    return HttpResponse("<h1>Add post</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Post Update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Post delete</h1>")
+
