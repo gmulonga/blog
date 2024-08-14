eda49695f1c15353b0405eb13a3485f45bc1460d
gmulonga <gadsonmulonga@gmail.com>
2024-08-14 16:01:20 +0300

    adding update func

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.post_list, name='list'),
     path('<int:id>/', views.post_detail, name='detail'),
     path('create/', views.post_add, name='create'),
-    path('update/', views.post_update, name='update'),
+    path('<int:id>/update/', views.post_update, name='update'),
     path('delete/', views.post_delete, name='delete'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     queryset = Post.objects.all()
@@ -18,10 +19,30 @@ def post_detail(request, id):
     return render(request, "post_detail.html", context)
 
 def post_add(request):
-    return HttpResponse("<h1>Add post</h1>")
+    # makes sure that the title and content is required
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
 
-def post_update(request):
-    return HttpResponse("<h1>Post Update</h1>")
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+def post_update(request, id):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Post delete</h1>")
